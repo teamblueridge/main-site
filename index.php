d8f6b6fe4794d10b929de521a4173dc2da5bdf06
HendrikBoone <hendrik.boone1@gmail.com>
2016-03-12 16:36:54 +0100

    Status checkup

--- index.php
+++ index.php
@@ -1,4 +1,27 @@
 <?php
+function checkOnline($domain) {
+   $curlInit = curl_init($domain);
+   curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
+   curl_setopt($curlInit,CURLOPT_HEADER,true);
+   curl_setopt($curlInit,CURLOPT_NOBODY,true);
+   curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
+
+   //get answer
+   $response = curl_exec($curlInit);
+
+   curl_close($curlInit);
+   if ($response) return false;
+   return true;
+}
+
+$blog = 'http://blog.teamblueridge.org';
+$code = 'http://github.com/teamblueridge';
+$gerrit = 'http://review.teamblueridge.org';
+$jenkins = 'http://jenkins.teamblueridge.org';
+$redmine = 'http://redmine.teamblueridge.org';
+$stikked = 'http://paste.teamblueridge.org';
+$down = '<code>This service is down!</code>';
+
 $pagetitle = 'Team BlueRidge - Simply Breathtaking';
 include 'header.php'; 
 ?>
@@ -26,7 +49,8 @@ include 'header.php';
                                 <i class="icon-picture"></i>
                                 <div class="service-desc">
                                     <h5>Redmine</h5>
-                                    <p><a href="https://redmine.teamblueridge.org">This platform</a> hosts all our projects, and also services as a wiki for all sorts of information.</p>
+                                    <p><a href="https://redmine.teamblueridge.org">This platform</a> hosts all our projects, and also services as a wiki for all sorts of information.
+                                    <?php if(checkOnline($redmine)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
@@ -37,7 +61,8 @@ include 'header.php';
                                 <i class="icon-refresh"></i>
                                 <div class="service-desc">
                                     <h5>Gerrit</h5>
-                                    <p><a href="https://review.teamblueridge.org">This code review</a> system allows anyone to easily push code changes to open source git projects.</p>
+                                    <p><a href="https://review.teamblueridge.org">This code review</a> system allows anyone to easily push code changes to open source git projects.
+                                    <?php if(checkOnline($gerrit)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
@@ -48,7 +73,8 @@ include 'header.php';
                                 <i class="icon-desktop"></i>
                                 <div class="service-desc">
                                     <h5><a href="">Jenkins</a></h5>
-                                    <p><a href="https://jenkins.teamblueridge.org">This buildbox</a> allows you to compile code on another machine then yours. Ideal for creating ROMs and such.</p>
+                                    <p><a href="https://jenkins.teamblueridge.org">This buildbox</a> allows you to compile code on another machine then yours. Ideal for creating ROMs and such.
+                                    <?php if(checkOnline($jenkins)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
@@ -61,7 +87,8 @@ include 'header.php';
                                 <i class="icon-gear"></i>
                                 <div class="service-desc">
                                     <h5>Stikked</h5>
-                                    <p><a href="https://paste.teamblueridge.org">A place</a> to share code, text, ... Simple, secure and fast. See also <a href="https://play.google.com/store/apps/details?id=org.teamblueridge.pasteitapp">Paste It</a>, its companion app.</p>
+                                    <p><a href="https://paste.teamblueridge.org">A place</a> to share code, text, ... Simple, secure and fast. See also <a href="https://play.google.com/store/apps/details?id=org.teamblueridge.pasteitapp">Paste It</a>, its companion app.
+                                    <?php if(checkOnline($stikked)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
@@ -72,7 +99,8 @@ include 'header.php';
                                 <i class="icon-rss"></i>
                                 <div class="service-desc">
                                     <h5>Blog</h5>
-                                    <p>Want to inform you on the recent news? You can read it on our <a href="https://blog.teamblueridge.org">blog</a>.</p>
+                                    <p>Want to inform you on the recent news? You can read it on our <a href="https://blog.teamblueridge.org">blog</a>.
+                                    <?php if(checkOnline($blog)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
@@ -83,7 +111,8 @@ include 'header.php';
                                 <i class="icon-code"></i>
                                 <div class="service-desc">
                                     <h5>Code</h5>
-                                    <p>All our projects are open source and hosted on <a href="https://github.com/teamblueridge">GitHub</a>. We are constantly working on new things.</p>
+                                    <p>All our projects are open source and hosted on <a href="https://github.com/teamblueridge">GitHub</a>. We are constantly working on new things.
+                                    <?php if(checkOnline($code)) {echo $down;}?></p>
                                 </div>
                             </div>
                         </div>
--- status.php
+++ status.php
@@ -1,5 +1,7 @@
 <?php
-//
+//Found in http://stackoverflow.com/questions/9817046/get-the-site-status-up-or-down
+//Only http allowed, if configured wrong on server side: https won't work
+
 function checkOnline($domain) {
    $curlInit = curl_init($domain);
    curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
@@ -14,5 +16,5 @@ function checkOnline($domain) {
    if ($response) return true;
    return false;
 }
-if(checkOnline('http://teamblueridge.org')) { echo "yes"; }
+if(checkOnline('http://google.com')) { echo "yes"; }
 ?>
