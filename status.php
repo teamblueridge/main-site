7a974cd3d4c25bf965366d9839e441a3bfc91451
HendrikBoone <hendrik.boone1@gmail.com>
2016-03-12 15:45:59 +0100

    Status script

---
+++ status.php
@@ -0,0 +1,18 @@
+<?php
+//
+function checkOnline($domain) {
+   $curlInit = curl_init($domain);
+   curl_setopt($curlInit,CURLOPT_CONNECTTIMEOUT,10);
+   curl_setopt($curlInit,CURLOPT_HEADER,true);
+   curl_setopt($curlInit,CURLOPT_NOBODY,true);
+   curl_setopt($curlInit,CURLOPT_RETURNTRANSFER,true);
+
+   //get answer
+   $response = curl_exec($curlInit);
+
+   curl_close($curlInit);
+   if ($response) return true;
+   return false;
+}
+if(checkOnline('http://teamblueridge.org')) { echo "yes"; }
+?>
